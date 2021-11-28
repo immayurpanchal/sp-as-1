5eee112d316e68c9596f2e4a7cf641bdde7e9630
Mayur Panchal <mayur.panchal@shoptimize.in>
2021-11-28 11:46:58 +0530

    feat: :sparkles: Fix input value not setting on intial load

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,33 +1,34 @@
-import React from 'react';
+import React from 'react'
 
-const Input = (props) => {
-	const { label, id, value, validate, errorMessage, ...restProps } = props;
+const Input = props => {
+  const { label, value, id, validate, errorMessage, ...restProps } = props
 
-	return (
-		<>
-			{label && (
-				<label htmlFor={id} className='col-sm-2 col-form-label'>
-					{label}
-				</label>
-			)}
-			<div className='col-sm-10'>
-				<input
-					{...restProps}
-					name={id}
-					className={`form-control `}
-					onInput={(e) => {
-						const isValidVal = !!props.validate(e);
-						if (!isValidVal) {
-							e.currentTarget.setCustomValidity(errorMessage);
-						} else {
-							e.currentTarget.setCustomValidity('');
-						}
-					}}
-				/>
-				<div className='invalid-feedback'>{errorMessage || ''}</div>
-			</div>
-		</>
-	);
-};
+  return (
+    <>
+      {label && (
+        <label htmlFor={id} className='col-sm-2 col-form-label'>
+          {label}
+        </label>
+      )}
+      <div className='col-sm-10'>
+        <input
+          {...restProps}
+          name={id}
+          value={value || ''}
+          className='form-control'
+          onInput={e => {
+            const isValidVal = !!props.validate(e)
+            if (!isValidVal) {
+              e.currentTarget.setCustomValidity(errorMessage)
+            } else {
+              e.currentTarget.setCustomValidity('')
+            }
+          }}
+        />
+        <div className='invalid-feedback'>{errorMessage || ''}</div>
+      </div>
+    </>
+  )
+}
 
-export default Input;
+export default Input
