ad130c90117924f4a9b9db83943c1aea0a1fb8d5
Mayur Panchal <mayur.panchal@shoptimize.in>
2021-11-28 10:48:09 +0530

    feat: :sparkles: Add useGooglePlace hook

---
+++ src/hooks/useGooglePlace.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+
+const useGooglePlace = () => {
+  const [place, setPlace] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  const handlePlaceSearch = latLng => {
+    console.log('Doing nothing with request latLng', latLng)
+    setLoading(true)
+    // Mock API call to fetch the Google Plus API response
+    setTimeout(() => {
+      setLoading(false)
+      setPlace('SP, Ahemedabad, Gujarat, India')
+    }, 500)
+  }
+
+  useEffect(() => {
+    setError(false)
+  }, [place])
+
+  return {
+    place,
+    loading,
+    error,
+    handlePlaceSearch
+  }
+}
+
+export default useGooglePlace
